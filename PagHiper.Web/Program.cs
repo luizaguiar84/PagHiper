8f1452ed2998caddeeb3d5777d1978fc0a3bb319
Luiz Aguiar <luizmanoel00@gmail.com>
2021-07-25 03:06:01 -0300

    Change Startup class

--- PagHiper.Web/Program.cs
+++ PagHiper.Web/Program.cs
@@ -20,7 +20,9 @@ namespace PagHiper.Web
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder.UseStartup<Startup>();
+					webBuilder
+					.UseKestrel()
+					.UseStartup<Startup>();
 				});
 	}
 }
