8e324e1a55ce57b008faeb95a587e05a88bf9ca0
Luiz Aguiar <luiz.aguiar@totvs.com.br>
2021-09-01 15:42:00 -0300

    Novas validações

--- src/PagHiper.Domain/Entities/Lead.cs
+++ src/PagHiper.Domain/Entities/Lead.cs
@@ -10,18 +10,18 @@ namespace PagHiper.Domain.Entities
         public DateTime DataCadastro { get; set; }
         public DateTime LastUpdate { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Inserir o nome")]
+        [MaxLength(100, ErrorMessage = "Tamanho do nome inválido, favor inserir um valor até 100 caracteres.")]
         public string Nome { get; set; }
 
         public bool AceitaPropaganda { get; set; }
 
-        [MaxLength(50)]
-        [DataType(DataType.EmailAddress)]
+        [MaxLength(50, ErrorMessage = "Tamanho do E-mail inválido!")]
+        [EmailAddress(ErrorMessage = "Por favor, insira um e-mail válido")]
         [DisplayName("E-Mail")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Favor inserir um número de telefone válido")]
         [MaxLength(14)]
         [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
