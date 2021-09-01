47677e498aee088409782ce8706b535ecfd8a78a
Luiz <43261487+luizaguiar84@users.noreply.github.com>
2021-08-31 23:43:16 -0300

    new fields in lead

--- src/PagHiper.Domain/Entities/Lead.cs
+++ src/PagHiper.Domain/Entities/Lead.cs
@@ -1,6 +1,6 @@
 using System;
 using System.ComponentModel;
-using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace PagHiper.Domain.Entities
 {
@@ -10,10 +10,23 @@ namespace PagHiper.Domain.Entities
         public DateTime DataCadastro { get; set; }
         public DateTime LastUpdate { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string Nome { get; set; }
 
+        public bool AceitaPropaganda { get; set; }
+
+        [MaxLength(50)]
+        [DataType(DataType.EmailAddress)]
         [DisplayName("E-Mail")]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(14)]
+        [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
+
+        [MaxLength(7)]
+        public string Cupom { get; set; }
     }
 }
--- src/PagHiper.Infra.MySql/Migrations/20210901023535_novosCamposEmLead.Designer.cs
+++ src/PagHiper.Infra.MySql/Migrations/20210901023535_novosCamposEmLead.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PagHiper.Infra.MySql.Context;
 
 namespace PagHiper.Infra.MySql.Migrations
 {
     [DbContext(typeof(MySqlCrudDbContext))]
-    partial class MySqlCrudDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210901023535_novosCamposEmLead")]
+    partial class novosCamposEmLead
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
