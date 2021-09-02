492c776c0796f25f8778fa8b892e391ee2d27dc1
Luiz Aguiar <luizmanoel00@gmail.com>
2021-09-02 20:09:04 -0300

    create repository

--- src/PagHiper.Infra/Repositories/LeadRepository.cs
+++ src/PagHiper.Infra/Repositories/LeadRepository.cs
@@ -1,9 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PagHiper.Domain.Entities;
+using PagHiper.Infra.Context;
 using PagHiper.Infra.Repositories.Interfaces;
 
 namespace PagHiper.Infra.Repositories
 {
     public class LeadRepository : ILeadRepository
     {
-        
+        private readonly CrudDbContext _context;
+
+        public LeadRepository(CrudDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<Lead> GetById(int id)
+        {
+            Lead lead = await _context.Leads
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            return lead;
+        }
+
+        public async Task<IEnumerable<Lead>> GetAll()
+        {
+            var leads = await _context.Leads.ToListAsync();
+            return leads;
+        }
+
+        public async Task<int> CreateNew(Lead lead)
+        {
+            lead.DataCadastro = DateTime.Now;
+            lead.LastUpdate = DateTime.Now;
+
+            var entity = _context.Add(lead);
+            await _context.SaveChangesAsync();
+
+            var id = entity.Entity.Id;
+            return id;
+        }
+
+        public async Task Delete(int id)
+        {
+            var lead = await GetById(id);
+
+            if (lead == null)
+                return;
+
+            _context.Leads.Remove(lead);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- src/PagHiper.Web/Controllers/LeadsController.cs
+++ src/PagHiper.Web/Controllers/LeadsController.cs
@@ -1,161 +1,136 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PagHiper.Domain.Entities;
-using PagHiper.Infra.Context;
+using PagHiper.Infra.Repositories.Interfaces;
 
 namespace PagHiper.Web.Controllers
 {
-	public class LeadsController : Controller
-	{
-		private readonly CrudDbContext _context;
+    public class LeadsController : Controller
+    {
+        private readonly ILeadRepository _leadRepository;
 
-		public LeadsController(CrudDbContext context)
-		{
-			_context = context;
-		}
+        public LeadsController(ILeadRepository leadRepository)
+        {
+            _leadRepository = leadRepository;
+        }
 
-		// GET: Leads
-		public async Task<IActionResult> Index()
-		{
-			return View(await _context.Leads.ToListAsync());
-		}
+        // GET: Leads
+        public async Task<IActionResult> Index()
+        {
+            var leads = await _leadRepository.GetAll();
+            return View(leads);
+        }
+
         public IActionResult Final()
         {
             return View();
         }
-		// GET: Leads/Details/5
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
 
-			var lead = await _context.Leads
-				.FirstOrDefaultAsync(m => m.Id == id);
-			if (lead == null)
-			{
-				return NotFound();
-			}
+        // GET: Leads/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var lead = await _leadRepository.GetById(id);
 
-			return View(lead);
-		}
+            if (lead == null)
+                return NotFound();
 
-		// GET: Leads/Create
-		public IActionResult Create()
-		{
-			return View();
-		}
+            return View(lead);
+        }
 
-		// POST: Leads/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("Nome,Email,Telefone,Id, Cupom, AceitaPropaganda")] Lead lead)
-		{
-			if (ModelState.IsValid)
-			{
-				//lead.Id = Guid.NewGuid();
-				lead.DataCadastro = DateTime.Now;
-				lead.LastUpdate = DateTime.Now;
+        // GET: Leads/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-				_context.Add(lead);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Final));
-			}
+        // POST: Leads/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,Email,Telefone,Id, Cupom, AceitaPropaganda")]
+            Lead lead)
+        {
+            await _leadRepository.CreateNew(lead);
+            return RedirectToAction(nameof(Final));
+        }
 
-			return View(lead);
-		}
+        // GET: Leads/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var lead = await _leadRepository.GetById(id);
 
-		// GET: Leads/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+            if (lead == null)
+                return NotFound();
 
-			var lead = await _context.Leads.FindAsync(id);
-			if (lead == null)
-			{
-				return NotFound();
-			}
-			return View(lead);
-		}
+            return View(lead);
+        }
 
-		// POST: Leads/Edit/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("DataCadastro, Nome,Email,Telefone,Id")] Lead lead)
-		{
-			if (id != lead.Id)
-			{
-				return NotFound();
-			}
+        // POST: Leads/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DataCadastro, Nome,Email,Telefone,Id")]
+            Lead lead)
+        {
+            if (id != lead.Id)
+            {
+                return NotFound();
+            }
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					lead.LastUpdate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    lead.LastUpdate = DateTime.Now;
+                    //_context.Update(lead);
+                    //await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LeadExists(lead.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
 
-					_context.Update(lead);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!LeadExists(lead.Id))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			return View(lead);
-		}
+                return RedirectToAction(nameof(Index));
+            }
 
-		// GET: Leads/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+            return View(lead);
+        }
 
-			var lead = await _context.Leads
-				.FirstOrDefaultAsync(m => m.Id == id);
-			if (lead == null)
-			{
-				return NotFound();
-			}
+        // GET: Leads/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var lead = await _leadRepository.GetById(id);
 
-			return View(lead);
-		}
+            if (lead == null)
+                return NotFound();
 
-		// POST: Leads/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			var lead = await _context.Leads.FindAsync(id);
-			_context.Leads.Remove(lead);
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
+            return View(lead);
+        }
 
-		private bool LeadExists(int id)
-		{
-			return _context.Leads.Any(e => e.Id == id);
-		}
-	}
+        // POST: Leads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _leadRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LeadExists(int id)
+        {
+            return true;
+            //return _context.Leads.Any(e => e.Id == id);
+        }
+    }
 }
